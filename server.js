ebb909bbe12301887613199ecda2073d470309a9
pukhrajp <pukhraj.prajapat@gmail.com>
2025-06-17 12:36:24 +0530

    login and verification

--- server.js
+++ server.js
@@ -26,11 +26,52 @@ app.get("/users", (req, res) => {
   res.send(usersWithoutPassword);
 });
 
+app.get("/verify-token", (req, res) => {
+  const authorizationValue = req.headers.authorization; // Assuming the token is sent in the Authorization header
+  const token = authorizationValue ? authorizationValue.split(" ")[1] : null; // Extract the token from the header
+  if (!token) {
+    return res.status(401).send("Unauthorized");
+  }
+  const user = db.users.find((u) => u.tokens && u.tokens.includes(token));
+  if (!user) {
+    return res.status(401).send("Invalid token");
+  }
+  const { password, tokens, ...restUser } = user; // Exclude password and tokens from the response
+  res.send({
+    message: "Token is valid",
+    user: restUser,
+  });
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = db.users.find(
+    (u) => u.email === email && u.password === password
+  );
+  if (!user) {
+    return res.status(401).send("Invalid email or password");
+  }
+  const token = v4(); // Generate a random token (in a real app, use JWT or similar)
+  if (user.tokens) {
+    user.tokens.push(token);
+  } else {
+    user.tokens = [token];
+  }
+
+  updateDatabase();
+  const { password: p, tokens, ...restUser } = user; // Exclude password from the response
+  res.send({
+    message: "Login successful",
+    user: restUser,
+    token,
+  });
+});
+
 app.post("/users", (req, res) => {
   const { name, email, password } = req.body;
   const user = db.users.find((u) => u.email === email);
   if (user) {
-    return res.status(400).send("User already exists with this email");
+    return res.status(400).send("User already exists with this email address");
   }
   const newUser = {
     id: v4(),
